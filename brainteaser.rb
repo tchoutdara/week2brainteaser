225116b5f6a86f056af774d8f9e5fcbfcf17c1c6
tchoutdara <monkeymilitia444@gmail.com>
2018-08-24 10:43:15 -0600

    clean up

--- brainteaser.rb
+++ brainteaser.rb
@@ -1,11 +1,43 @@
-@arr= []
+#My attempt
 
 def user_input
-input = gets.strip
-letter = input.chars.unshift
-print letter
+    puts "Enter a word"
+    @word = gets.strip.downcase.gsub(" ", "").split("")
+    palindrome_check
+end
+
+def palindrome_check
+    reverse = []
+    @word.each { |letter| reverse.unshift(letter) }
+  if @word == reverse
+    puts "it's a palindrome."
+  else
+    puts "Not a palindrome."
+  end
+  user_input
 end
 
 user_input
 
 #Solution
+# require 'pry'
+# require 'colorize'
+
+# def main
+#   print `clear`
+#   puts "----palindrome checker----".green
+#   @word = gets.strip.downcase.gsub(" ", "").split("")
+#   check
+# end
+
+# def check
+#   reverse = []
+#   @word.each { |letter| reverse.unshift(letter) }
+#   if @word == reverse
+#     puts "it's a palindrome.".green
+#   else
+#     puts "nope.".red
+#   end
+# end
+
+# main
